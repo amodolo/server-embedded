8cc5a748b679848c21ed87d4e3eb44c893d29aaa
Alessandro Modolo <alessandro.modolo@overit.it>
2021-12-03 00:35:05 +0100

    context working in case of: - annotated servlet - web.xml servlet
    web-fragment not tested
    
    static resources are loaded (also inside webjar)

--- app/src/main/java/org/example/app/Launcher.java
+++ app/src/main/java/org/example/app/Launcher.java
@@ -3,7 +3,7 @@ package org.example.app;
 public class Launcher {
 
     public static void main(String[] args) throws Exception {
-        WebServer server = new TomcatServer(8080, "/");
+        WebServer server = new TomcatServer(8080, "/context");
 //        server = new JettyServer(8080);
         server.start();
 
--- app/src/main/java/org/example/app/TomcatServer.java
+++ app/src/main/java/org/example/app/TomcatServer.java
@@ -3,15 +3,20 @@ package org.example.app;
 import org.apache.catalina.Context;
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.core.StandardHost;
+import org.apache.catalina.startup.Constants;
 import org.apache.catalina.startup.ContextConfig;
+import org.apache.catalina.startup.ExpandWar;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.buf.UriUtil;
 import org.example.core.servlet.ContextListener;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class TomcatServer implements WebServer {
     private Tomcat server;
@@ -31,27 +36,28 @@ public class TomcatServer implements WebServer {
         server.setBaseDir(createTempDir("tomcat").getAbsolutePath());
         server.setPort(port);
         server.getHost().setAutoDeploy(false);
-        if (server.getHost() instanceof StandardHost) {
-            ((StandardHost) server.getHost()).setUnpackWARs(false);
-        }
 
-        server.getHost().setAppBase(".");
-        File docBase = createTempDir("tomcat-docbase");
-        Context ctx = server.addContext(server.getHost(), "/", docBase.getAbsolutePath());
+        File docBase = new File(server.getHost().getAppBaseFile(), contextPath);
 
+        Context ctx = server.addContext(server.getHost(), contextPath, docBase.getAbsolutePath());
 
         ctx.setParentClassLoader(getClass().getClassLoader());
 
+        server.getHost().getAppBaseFile().mkdir();
+        URL thisJar = getClass().getClassLoader().getResource("/");
+        assert thisJar != null;
+        ExpandWar.expand(server.getHost(), thisJar, contextPath);
 
-        URL webXML = getClass().getClassLoader().getResource("/WEB-INF/web.xml");
-        if (webXML != null) {
-            File webInf = new File(docBase, "WEB-INF");
-            webInf.mkdir();
-            File destination = new File(webInf, "web.xml");
-            Files.copy(webXML.openStream(), destination.toPath());
-            ctx.addLifecycleListener(new ContextConfig());
-        }
 
+        ContextConfig contextConfig = new ContextConfig();
+        contextConfig.setDefaultWebXml(server.noDefaultWebXmlPath());
+        server.getHost().setConfigClass(contextConfig.getClass().getName());
+        ctx.addLifecycleListener(contextConfig);
+        ctx.setConfigFile(getWebappConfigFile(docBase.getAbsolutePath(), contextPath));
+
+
+        ctx.addApplicationListener(ContextListener.class.getName());
+        ctx.addLifecycleListener(server.getDefaultWebXmlListener());
         ctx.addLifecycleListener(event -> {
             if (Lifecycle.AFTER_START_EVENT.equals(event.getType())) {
                 long elapsed = System.currentTimeMillis() - start;
@@ -59,7 +65,6 @@ public class TomcatServer implements WebServer {
             }
         });
 
-        ctx.addApplicationListener(ContextListener.class.getName());
 
         server.start();
 
@@ -92,5 +97,42 @@ public class TomcatServer implements WebServer {
             throw new RuntimeException(
                 "Unable to create tempDir. java.io.tmpdir is set to " + System.getProperty("java.io.tmpdir"), ex);
         }
+    }
+
+    protected URL getWebappConfigFile(String path, String contextName) {
+        File docBase = new File(path);
+        if (docBase.isDirectory()) {
+            return getWebappConfigFileFromDirectory(docBase, contextName);
+        } else {
+            return getWebappConfigFileFromWar(docBase, contextName);
+        }
+    }
+
+    private URL getWebappConfigFileFromDirectory(File docBase, String contextName) {
+        URL result = null;
+        File webAppContextXml = new File(docBase, Constants.ApplicationContextXml);
+        if (webAppContextXml.exists()) {
+            try {
+                result = webAppContextXml.toURI().toURL();
+            } catch (MalformedURLException e) {
+//                Logger.getLogger(getLoggerName(getHost(), contextName)).log(Level.WARNING,
+//                    "Unable to determine web application context.xml " + docBase, e);
+            }
+        }
+        return result;
+    }
+
+    private URL getWebappConfigFileFromWar(File docBase, String contextName) {
+        URL result = null;
+        try (JarFile jar = new JarFile(docBase)) {
+            JarEntry entry = jar.getJarEntry(Constants.ApplicationContextXml);
+            if (entry != null) {
+                result = UriUtil.buildJarUrl(docBase, Constants.ApplicationContextXml);
+            }
+        } catch (IOException e) {
+//            Logger.getLogger(getLoggerName(getHost(), contextName)).log(Level.WARNING,
+//                "Unable to determine web application context.xml " + docBase, e);
+        }
+        return result;
     }
 }
